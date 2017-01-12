707d292bc03f65e894ec5b1faf98594adf3fbce4
lulubun <laurapetitt@gmail.com>
2017-01-12 10:01:10 -0500

    all CRUD options working from POSTman

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-//const {DATABASE_URL, PORT} = require('./config');
+const {DATABASE_URL, PORT} = require('./config');
 const {Tasting}= require('./models');
 
 const app = express();
@@ -11,6 +11,7 @@ const app = express();
 mongoose.Promise = global.Promise;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
 	res.json('hello world');
 });
@@ -40,37 +41,57 @@ app.get('/aficionado/:id', (req, res) => {
 });
 
 app.post('/aficionado', (req, res) => {
-  const requiredFields = ['id', 'teaName', 'date', 'vendor', 'teaType', 'amountUsed', 'waterUsed', 'brewTemp', 'steepingTime', 'additions', 'aroma', 'taste', 'stars', 'notes'];
+  const requiredFields = ['teaName', 'date'];
   requiredFields.forEach(field => {
     if (!(field in req.body)) {
       res.status(400).json(
         {error: `Missing "${field}" in request body`});
     }});
 
+  var vendor = req.body.vendor ? req.body.vendor : '';
+  var teaType = req.body.teaType ? req.body.teaType : {};
+  var teaColor = teaType.teaColor ? teaType.teaColor : '';
+  var flavored = teaType.flavored ? teaType.flavored : false;
+  var amountUsed = req.body.amountUsed ? req.body.amountUsed : '';
+  var waterUsed = req.body.waterUsed ? req.body.waterUsed : '';
+  var brewTemp = req.body.brewTemp ? req.body.brewTemp : '212 (rolling boil)';
+  var steepingTime = req.body.steepingTime ? req.body.steepingTime : '';
+  var additions = req.body.additions ? req.body.additions : {};
+  var cream = additions.cream ? additions.cream : false;
+  var sugar = additions.sugar ? additions.sugar : '';
+  var honey = additions.honey ? additions.honey : false;
+  var lemon = additions.lemon ? additions.lemon : false;
+  var other = additions.other ? additions.other : '';
+  var aroma = req.body.aroma ? req.body.aroma : '';
+  var taste = req.body.taste ? req.body.taste : '';
+  var stars = req.body.stars ? req.body.stars : 0;
+  var notes = req.body.notes ? req.body.notes : '';
+
+
   Tasting
     .create({
     	teaName: req.body.teaName,
     	date: req.body.date,
-    	vendor: req.body.vendor,
+    	vendor: vendor,
     	teaType: {
-    		teaColor: req.body.teaType.teaColor,
-    		flavored: req.body.teaType.flavored
+    		teaColor: teaColor,
+    		flavored: flavored
     	},
-    	amountUsed: req.body.amountUsed,
-    	waterUsed: req.body.waterUsed,
-    	brewTemp: req.body.brewTemp,
-    	steepingTime: req.body.steepingTime,
+    	amountUsed: amountUsed,
+    	waterUsed: waterUsed,
+    	brewTemp: brewTemp,
+    	steepingTime: steepingTime,
     	additions: {
-    		cream: req.body.additions.cream,
-    		sugar: req.body.additions.sugar,
-    		honey: req.body.additions.honey,
-    		lemon: req.body.additions.lemon,
-    		other: req.body.additions.other
+    		cream: cream,
+    		sugar: sugar,
+    		honey: honey,
+    		lemon: lemon,
+    		other: other
     	},
-    	aroma: req.body.aroma,
-    	taste: req.body.taste,
-    	stars: req.body.stars,
-    	notes: req.body.notes
+    	aroma: aroma,
+    	taste: taste,
+    	stars: stars,
+    	notes: notes
     })
     .then(taste => res.status(201).json(taste.apiRepr()))
     .catch(err => {
@@ -135,6 +156,48 @@ app.use('*', function(req, res) {
 app.get('/newentry', (req, res) => {
 	res.json('hello world');
 });
-app.listen(process.env.PORT || 8080);
 
-module.exports = app;
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {runServer, app, closeServer};
